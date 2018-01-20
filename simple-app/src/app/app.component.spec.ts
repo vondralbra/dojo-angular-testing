4a98d47f5271608a1421c8e967529cc1ab2639ed
vondralbra <vondralbra@gmx.de>
2018-01-20 11:45:33 +0100

    Fixed automatic created test specs by adding imports of all used components

--- simple-app/src/app/app.component.spec.ts
+++ simple-app/src/app/app.component.spec.ts
@@ -1,4 +1,7 @@
 import { RootComponentComponent } from './components/root-component/root-component.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -9,7 +12,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent, RootComponentComponent
+        AppComponent, RootComponentComponent, Component1Component, Component2Component, Component3Component
       ],
     }).compileComponents();
   }));
--- simple-app/src/app/components/root-component/root-component.component.spec.ts
+++ simple-app/src/app/components/root-component/root-component.component.spec.ts
@@ -3,6 +3,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RootComponentComponent } from './root-component.component';
+import { Component1Component } from '../component1/component1.component';
+import { Component2Component } from '../component2/component2.component';
+import { Component3Component } from '../component3/component3.component';
 
 describe('RootComponentComponent', () => {
   let component: RootComponentComponent;
@@ -10,7 +13,7 @@ describe('RootComponentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RootComponentComponent]
+      declarations: [ RootComponentComponent, Component1Component, Component2Component, Component3Component]
     })
     .compileComponents();
   }));
